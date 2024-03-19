5fcf83567028cc12f33e2678afb910a7c44636c4
Abdallah-Adroit100 <alhussaa@uoguelph.ca>
2024-03-18 23:16:21 -0400

    Merge remote-tracking branch 'Game2D/master'

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -9,57 +9,42 @@ public class CollisionChecker {
 
     }
 
-    public void checkTile(PlayerEntity entity){
+    public boolean checkTile(PlayerEntity entity, String direction) {
+        int nextX = entity.worldX;
+        int nextY = entity.worldY;
 
-        int entityLeftWorldX = entity.worldX + entity.solidArea.x;
-        int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width ;
-
-        int entityTopWorldY = entity.worldY + entity.solidArea.y;
-        int entityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height;
-
-        int entityLeftCol= entityLeftWorldX/gp.tileSize;
-        int entityRightCol= entityRightWorldX/gp.tileSize;
-        int entityTopRow= entityTopWorldY/gp.tileSize;
-        int entityBottomRow= entityBottomWorldY/gp.tileSize;
-
-        int tileNum1, tileNum2;
-
-        switch(entity.direction){
-
-            case "up":
-                entityTopRow= (entityTopWorldY-entity.speed)/gp.tileSize;
-                tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityTopRow];
-                tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityTopRow];
-                if(gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision){
-                    entity.collisionOn = true;
-                }
-                break;
-            case "down":
-                entityBottomRow= (entityBottomWorldY+entity.speed)/gp.tileSize;
-                tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
-                tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
-                if(gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision){
-                    entity.collisionOn = true;
-                }
-                break;
-            case "left":
-                entityLeftCol= (entityLeftWorldX-entity.speed)/gp.tileSize;
-                tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityTopRow];
-                tileNum2 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
-                if(gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision){
-                    entity.collisionOn = true;
-                }
-                break;
-            case "right":
-                entityRightCol= (entityRightWorldX+entity.speed)/gp.tileSize;
-                tileNum1 = gp.tileM.mapTileNum[entityRightCol][entityTopRow];
-                tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
-                if(gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision){
-                    entity.collisionOn = true;
-                }
-                break;
+        if (direction.equals("up")) {
+            nextY = entity.worldY - entity.speed;
+        } else if (direction.equals("down")) {
+            nextY = entity.worldY + entity.speed;
+        } else if (direction.equals("left")) {
+            nextX = entity.worldX - entity.speed;
+        } else if (direction.equals("right")) {
+            nextX = entity.worldX + entity.speed;
         }
 
+        // Convert nextX and nextY to tile positions
+        int tileX = nextX / gp.tileSize;
+        int tileY = nextY / gp.tileSize;
 
+        // Check if the tile at the new position is solid
+        if (gp.tileM.tile[gp.tileM.mapTileNum[tileX][tileY]].collision) {
+            return true;
+        }
+
+        return false;
     }
+    public boolean checkTileCollision(PlayerEntity entity, int targetX, int targetY) {
+        // Calculate the future position's tile coordinates
+        int tileX = targetX / gp.tileSize;
+        int tileY = targetY / gp.tileSize;
+
+        // Check if the tile at the future position is solid
+        if (gp.tileM.tile[gp.tileM.mapTileNum[tileX][tileY]].collision) {
+            return true; // Collision detected
+        }
+
+        return false; // No collision
+    }
+
 }
